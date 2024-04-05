fd65f69a8d635d9971ea5650d1c759c72fda11d1
reidspencer <reid.spencer@yoppworks.com>
2024-04-04 19:58:12 -0700

    Fix a bug caused by re-ordering output

--- hugo/src/main/scala/com/ossuminc/riddl/hugo/writers/DomainWriter.scala
+++ hugo/src/main/scala/com/ossuminc/riddl/hugo/writers/DomainWriter.scala
@@ -3,14 +3,13 @@ package com.ossuminc.riddl.hugo.writers
 import com.ossuminc.riddl.diagrams.mermaid.DomainMapDiagram
 import com.ossuminc.riddl.language.AST.*
 
-trait DomainWriter { this: MarkdownWriter => 
+trait DomainWriter { this: MarkdownWriter =>
 
   def emitDomain(domain: Domain, parents: Seq[String]): Unit = {
     val diagram = DomainMapDiagram(domain)
 
     containerHead(domain, "Domain")
     emitDefDoc(domain, parents)
-    emitAuthorInfo(domain.authors)
     h2("Domain Map")
     emitMermaidDiagram(diagram.generate)
     definitionToc("Subdomains", domain.domains)
@@ -19,6 +18,7 @@ trait DomainWriter { this: MarkdownWriter =>
     definitionToc("Epics", domain.epics)
     emitTypesToc(domain)
     emitUsage(domain)
+    emitAuthorInfo(domain.authors)
     emitTerms(domain.terms)
     emitIndex("Domain", domain, parents)
   }
